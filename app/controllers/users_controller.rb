c2e9d48a11b1915339e3468169f3b6cf733189b5
Michael Chong <mikec0920@gmail.com>
2022-06-23 16:57:03 -0700

    Generate edit user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -8,14 +13,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to new_user_path, alert: "User created successfully."
     else
+      flash.now[:error] = "Invalid user input"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path(:id)
+    else
+      render :edit
     end
   end
 
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
     end
 end
